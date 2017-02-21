7becfc51a4287ad040557446be75a9d2ba3e11df
MURILO FUJITA <murilo.fujita@senacsp.edu.br>
2017-02-21 17:17:47 -0300

    Commit inicial em 21/02/2017.

---
+++ Assets/Apuracao.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Apuracao : principal {
+
+	public Text mostraSituacao;
+	public int acertos = 0;
+	public int erros = 0;
+
+	// Use this for initialization
+	void Start () 
+	{
+		contador = contador - 1;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown (KeyCode.A)) 
+		{
+			if (pilha [contador] == "Laranja") 
+			{
+				mostraSituacao.text = "Acertou Miseravi!";
+				contador = contador - 1;
+				acertos = acertos + 1;
+			} 
+			else 
+			{
+				mostraSituacao.text = "Errou!!!";
+				erros = erros + 1;
+			}
+		}
+
+		if (Input.GetKeyDown (KeyCode.S)) 
+		{
+			if (pilha [contador] == "Vermelha") 
+			{
+				mostraSituacao.text = "Acertou Miseravi!";
+				contador = contador - 1;
+				acertos = acertos + 1;
+			} 
+			else 
+			{
+				mostraSituacao.text = "Errou!!!";
+				erros = erros + 1;
+			}
+		}
+
+		if (Input.GetKeyDown (KeyCode.D)) 
+		{
+			if (pilha [contador] == "Verde") 
+			{
+				mostraSituacao.text = "Acertou Miseravi!";
+				contador = contador - 1;
+				acertos = acertos + 1;
+			} 
+			else 
+			{
+				mostraSituacao.text = "Errou!!!";
+				erros = erros + 1;
+			}
+		}
+
+		if (erros > 2) 
+		{
+			SceneManager.LoadScene ("cena-final");
+		}
+
+		if (acertos == 5) 
+		{
+			SceneManager.LoadScene ("cena-final-vitoria");
+		}
+	}
+}
---
+++ Assets/principal.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class principal : MonoBehaviour {
+
+	public static int contador;
+	public static string[] pilha;
+	public Text contaJogadas;
+
+	// Use this for initialization
+	void Start () 
+	{
+		pilha = new string[10];
+		contador = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown (KeyCode.A)) 
+		{
+			pilha [contador] = "Laranja";
+			Debug.Log (pilha [contador]);
+
+			contador = contador + 1;
+
+			if (contador == 5) 
+			{
+				SceneManager.LoadScene ("cena2");
+			}
+		}
+
+		if (Input.GetKeyDown (KeyCode.S)) 
+		{
+			pilha [contador] = "Vermelha";
+			Debug.Log (pilha [contador]);
+
+			contador = contador + 1;
+
+			if (contador == 5) 
+			{
+				SceneManager.LoadScene ("cena2");
+			}
+		}
+
+		if (Input.GetKeyDown (KeyCode.D)) 
+		{
+			pilha [contador] = "Verde";
+			Debug.Log (pilha [contador]);
+
+			contador = contador + 1;
+
+			if (contador == 5) 
+			{
+				SceneManager.LoadScene ("cena2");
+			}
+		}
+
+		contaJogadas.text = contador.ToString ();		
+	}
+}
